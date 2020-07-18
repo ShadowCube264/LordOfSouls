94bb43480bad8962ed1421f1d15916d5e3aa7983
BuildTools <unconfigured@null.spigotmc.org>
2020-07-18 19:34:03 +0100

    Start programming the mod

---
+++ LordOfSouls.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Modding;
+using UnityEngine;
+
+namespace LordOfSouls
+{
+    public class LordOfSouls : Mod {
+        public override string GetVersion() => "1.0.0";
+        public override void Initialize(){
+            Log("Idk how to code lol");
+        }
+    }
+}
