0b49ba02c9cd69af4091d018da891f5f8a0a5ead
ShadowCube264 <68464043+ShadowCube264@users.noreply.github.com>
2020-07-18 21:21:24 +0100

    Fix and clean up LordOfSouls.cs

--- LordOfSouls.cs
+++ LordOfSouls.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 namespace LordOfSouls
 {
-    public class LordOfSouls : Mod {
-        public override string GetVersion() => "1.0.0";
-        public override void Initialize(){
-            Log("Idk how to code lol");
+    public class LordOfSouls : Mod
+    {
+        public static void Main()
+        {
+            public override string GetVersion() => "1.0.0";
+            public override void Initialize()
+            {
+                Log("Idk how to code lol");
+            }
         }
     }
 }
