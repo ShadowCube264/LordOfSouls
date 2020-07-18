2aab110c44f8eb17410347c97723e4b25c76e7e5
BuildTools <unconfigured@null.spigotmc.org>
2020-07-18 12:13:36 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LordOfSouls
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
