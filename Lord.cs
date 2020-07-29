2c155c17791a06d63d662a0e68d7c951070d9d5e
ShadowCube264 <lhofalvi@hotmail.com>
2020-07-29 19:15:38 +0100

    Fix errors in Lord.cs

--- Lord.cs
+++ Lord.cs
@@ -8,8 +8,8 @@ namespace LordOfSouls
 {
     internal class Lord : MonoBehaviour
     {
-        GameObject lord = Instantiate(GameObject.Find("Mage Lord"));
-        PlayMakerFSM fsm = go.LocateMyFSM("Dream Mage Lord-Mage Lord");
-        var testAction = fsm.GetAction<SetVelocity2d>("Quake Down", 3).y = 0.5f;
+        static GameObject lord = Instantiate(GameObject.Find("Mage Lord"));
+        static PlayMakerFSM fsm = lord.LocateMyFSM("Dream Mage Lord-Mage Lord");
+        static FsmFloat testAction = fsm.GetAction<SetVelocity2d>("Quake Down", 3).y = 0.5f;
     }
 }
